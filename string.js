a7134d1a21c1f108ce64e8e774bc47d1fc092ada
kadekframa <dframa19@gmail.com>
2023-01-15 21:54:09 +0800

    Standard Library JS String.

---
+++ string.js
@@ -0,0 +1,20 @@
+// Standard Library JavaScript String.
+
+{
+  const name = "Kadek Frama Danamastyana";
+
+  console.info(name.length);
+  console.info(name.toLowerCase());
+  console.info(name.toUpperCase());
+  console.info(name.split(" "));
+
+  console.info(name);
+
+  const input = "    input data   ";
+  console.info(input.trim());
+  console.info(input);
+
+  const number = 12345;
+  console.info(number.toString()); // string.
+  console.info(typeof number.toString());
+}
